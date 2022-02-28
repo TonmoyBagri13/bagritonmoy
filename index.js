6ec1b94d7672fc818e2be03853f700a5fb77df0b
TonmoyBagri13 <toba3185@gmail.com>
2022-02-28 14:35:46 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/*let num1 = 1;
+let num2 = 2;
+let sum = num1+num2
+console.log("sum : " + sum);
+
+let name1 = 'John';
+let name2 = name1;
+
+name2 = 'Doe';
+
+console.log('name1 : ' + name1);
+console.log('name2 : ' + name2);
+
+
+let person1 = {
+    name : 'John'
+}
+
+let person2 = person1;
+
+person2.name = 'Doe';
+
+console.log('person1 name : ' + person1.name);
+console.log('person2 name : ' + person2.name);
+let rollNum = 2561;
+let firstName = 'Tonmoy';
+let secondName = 'Bagri';
+let fatherName = 'Tapan Kumar Bagri';
+let motherName = 'Rima Bagri';
+let loverName = 'Debopriya Das';
+
+let student ={
+    rollNum :'My roll no is  2561',
+    myName : 'My name is Tonmoy Bagri',
+     fatherName : 'My fathers name is Tapan Kumar Bagri',
+    motherName : 'My mother name is Rima Bagri',
+    loverName : 'My lover name is Debopriya Das'
+
+
+}
+console.log(student.firstName);
+console.log(student.secondName);
+console.log(student.rollNum);
+console.log(student.fatherName);
+console.log(student.motherName);
+console.log(student.loverName);
+let name = ['Tonmoy','Bagri'];
+name[2]=['Bhattacharya'];
+console.log(name);
+name[2]=[1];
+console.log(name)*/
+function paglachoda(){
+    console.log('I am a good boy');
+
+}
+paglachoda();
