6aa126bb1c22c06e5c9bdbbe839a0757577e58ef
TonmoyBagri13 <toba3185@gmail.com>
2022-03-09 12:22:17 +0530

    y_deconstructing_commit

---
+++ deconstructing.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4,5];
+
+
+/*const first = arr[0];
+const second = arr[1];
+const third = arr[2];
+
+console.log(first + "," + second + "," + third);*/
+
+//using destructuring concept
+let [th,four,fif] = arr;
+
+console.log("using deconstructor method");
+
+console.log(th);
